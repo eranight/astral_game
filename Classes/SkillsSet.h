286ca7628db4fa8110b42e317127f4c06a8058af
eranight <abelnigt@gmail.com>
2017-08-30 22:35:04 +0300

    Modified the SkillsSet class (not working yet)

--- Classes/SkillsSet.h
+++ Classes/SkillsSet.h
@@ -1,6 +1,9 @@
 #ifndef __SKILLSSET_H__
 #define __SKILLSSET_H__
 
+#include <vector>
+#include <memory>
+
 #include "Controller.h"
 #include "Skill.h"
 
@@ -8,7 +11,14 @@ namespace astral_game
 {
 	class SkillsSet : public Controller
 	{
-		
+	protected:
+		SkillsSet() = default;
+	public:
+		static SkillsSet * create();
+		bool init() override;
+		void update(float dt) override;
+	public:
+		std::vector<std::shared_ptr<Skill>> vectorOfSkills;
 	};
 }
 
