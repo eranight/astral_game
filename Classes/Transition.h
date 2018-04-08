a07bfdeaa8b31579eb2bf25e1c9c53e8d3f13d3f
eranight <abelnigt@gmail.com>
2018-04-08 21:37:51 +0300

    added implementation of the Transition class

--- Classes/Transition.h
+++ Classes/Transition.h
@@ -1,12 +1,23 @@
 #ifndef __TRANSITION_H__
 #define __TRANSITION_H__
 
+#include <memory>
+#include "Condition.h"
+
 namespace astral_game
 {
+	class State;
 
 	class Transition
 	{
-
+	public:
+		Transition(std::shared_ptr<Condition> condition, std::shared_ptr<State> targetState) : condition(condition), targetState(targetState) {};
+	public:
+		bool test() { return condition->test(); }
+		std::shared_ptr<State> getTargetState() { return targetState; }
+	private:
+		std::shared_ptr<Condition> condition;
+		std::shared_ptr<State> targetState;
 	};
 
 }
