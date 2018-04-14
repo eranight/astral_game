65cc3bdfe9276e71421fbf31ce6f11333e73467d
eranight <abelnigt@gmail.com>
2018-04-14 12:35:32 +0300

    added simple implementation of the StateMachine class

---
+++ Classes/StateMachine.cpp
@@ -0,0 +1,22 @@
+#include "StateMachine.h"
+#include "State.h"
+
+using namespace astral_game;
+
+void StateMachine::update(float dt) {
+	std::shared_ptr<Transition> triggeredTransition = nullptr;
+	for (auto & transition : currentState->getTransitions()) {
+		if (transition->isTriggered()) {
+			triggeredTransition = transition;
+			break;
+		}
+	}
+	if (triggeredTransition != nullptr) {
+		currentState->exit();
+		currentState = triggeredTransition->getTargetState();
+		currentState->entry();
+	}
+	else {
+		currentState->update(dt);
+	}
+}
--- Classes/StateMachine.h
+++ Classes/StateMachine.h
@@ -1,14 +1,23 @@
 #ifndef __STATE_MACHINE_H__
 #define __STATE_MACHINE_H__
 
+#include <vector>
+#include <memory>
+
 #include "Updatable.h"
 
 namespace astral_game
 {
+	class State;
 
 	class StateMachine : public Updatable
 	{
-
+	public:
+		virtual void update(float dt) override;
+	private:
+		std::vector<std::shared_ptr<State>> states;
+		std::shared_ptr<State> currentState;
+		std::shared_ptr<State> initialState;
 	};
 
 }
